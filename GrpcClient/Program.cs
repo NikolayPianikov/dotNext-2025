5e76ff21b0ef15d57ee6d99ac95fa086b4b069ea
Nikolay Pianikov <nikolayp@mail.ru>
2025-04-24 11:52:28 +0300

    Grpc start

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,17 @@
+﻿var composition = new Composition();
+var root = composition.Root;
+
+await root.Run();
+
+internal partial class Program(
+    IConsole console)
+{
+    private async Task Run()
+    {
+        while (!console.IsKeyAvailable)
+        {
+            // var reply = await client.GetNowAsync(new NowRequest());
+            // console.Write($"{reply.Date} {reply.Time}");
+        }
+    }
+}
