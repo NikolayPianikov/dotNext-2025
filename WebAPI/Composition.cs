2c39458fef3e41bd6606e42b513da0272b78c5da
Nikolay Pianikov <nikolayp@mail.ru>
2025-04-24 11:52:28 +0300

    WebAPI start

---
+++ WebAPI/Composition.cs
@@ -0,0 +1,16 @@
+namespace WebAPI;
+
+internal partial class Composition: ServiceProviderFactory<Composition>
+{
+    [Conditional("DI")]
+    private void Setup() => DI.Setup()
+        .DependsOn(Base)
+
+        .Bind().As(Singleton).To<ClockViewModel>()
+        .Bind().To<ClockModel>()
+        .Bind().As(Singleton).To<Ticks>()
+
+        // Infrastructure
+        .Bind().To<MicrosoftLoggerAdapter<TT>>()
+        .Bind().To<CurrentThreadDispatcher>();
+}
