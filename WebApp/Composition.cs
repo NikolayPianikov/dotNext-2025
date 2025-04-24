6b25085df899fa0b73aba1fb32d663f67dcc50fd
Nikolay Pianikov <nikolayp@mail.ru>
2025-04-24 11:52:28 +0300

    Web start

---
+++ WebApp/Composition.cs
@@ -0,0 +1,14 @@
+namespace WebApp;
+
+internal partial class Composition
+{
+    [Conditional("DI")]
+    private void Setup() => DI.Setup()
+        .Bind().As(Singleton).To<ClockViewModel>()
+        .Bind().To<ClockModel>()
+        .Bind().As(Singleton).To<Ticks>()
+
+        // Infrastructure
+        .Bind().To<MicrosoftLoggerAdapter<TT>>()
+        .Bind().To<CurrentThreadDispatcher>();
+}
