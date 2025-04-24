9f6289a82ea78ce17bdd79ae5ed3fb89861b5efb
Nikolay Pianikov <nikolayp@mail.ru>
2025-04-24 11:52:28 +0300

    Blazor finish

---
+++ BlazorServerApp/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using System.Diagnostics;
+global using Clock;
+global using Pure.DI;
+global using Pure.DI.MS;
+global using static Pure.DI.Lifetime;
---
+++ BlazorWebAssemblyApp/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using System.Diagnostics;
+global using BlazorWebAssemblyApp;
+global using Clock;
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using Pure.DI;
+global using Pure.DI.MS;
+global using static Pure.DI.Lifetime;
