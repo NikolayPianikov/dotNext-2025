7c16243864ed3cc8d1d593452a0276dfda80aae2
Nikolay Pianikov <nikolayp@mail.ru>
2025-04-24 11:52:28 +0300

    MinimalWebAPI start

---
+++ MinimalWebAPI/Composition.cs
@@ -0,0 +1,21 @@
+namespace MinimalWebAPI;
+
+internal partial class Composition : ServiceProviderFactory<Composition>
+{
+    // IMPORTANT:
+    // Only composition roots (regular or anonymous) can be resolved through the `IServiceProvider` interface.
+    // These roots must be registered using `Root(...)` or `RootBind()` calls.
+    [Conditional("DI")]
+    private void Setup() => DI.Setup()
+        .DependsOn(Base)
+
+        .Root<IClockViewModel>()
+
+        .Bind().To<ClockViewModel>()
+        .Bind().To<ClockModel>()
+        .Bind().As(Singleton).To<Ticks>()
+
+        // Infrastructure
+        .Bind().To<MicrosoftLoggerAdapter<TT>>()
+        .Bind().To<CurrentThreadDispatcher>();
+}
