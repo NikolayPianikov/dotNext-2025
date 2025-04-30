835366783847f5ce22053da4896133b42570829b
Nikolay Pianikov <nikolayp@mail.ru>
2025-04-30 14:04:41 +0300

    Console start

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿return;
+
+internal partial class Program(
+    ITicks ticks,
+    IConsole console,
+    IClockModel clock)
+{
+    private void Run()
+    {
+        ticks.Tick += OnTick;
+        while (!console.IsKeyAvailable);
+        ticks.Tick -= OnTick;
+    }
+
+    private void OnTick()
+    {
+        var now = clock.Now;
+        console.Write($"{now:d} {now:T}");
+    }
+}
