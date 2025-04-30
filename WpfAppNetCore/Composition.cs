d5fd7b4a0028308a92b582246f487680781c59e1
Nikolay Pianikov <nikolayp@mail.ru>
2025-04-30 16:34:21 +0300

    WPF finish

--- WpfAppNetCore/Composition.cs
+++ WpfAppNetCore/Composition.cs
@@ -4,10 +4,16 @@ internal partial class Composition
 {
     [Conditional("DI")]
     private void Setup() => DI.Setup()
+        .Hint(Hint.Resolve, "Off")
+
         .Root<IAppViewModel>(nameof(App))
         .Root<IClockViewModel>(nameof(Clock))
 
         .Bind().As(Singleton).To<ClockViewModel>()
         .Bind().To<ClockModel>()
-        .Bind().As(Singleton).To<Ticks>();
+        .Bind().As(Singleton).To<Ticks>()
+
+        // Infrastructure
+        .Bind().To<DebugLog<TT>>()
+        .Bind().To<WpfDispatcher>();
 }
