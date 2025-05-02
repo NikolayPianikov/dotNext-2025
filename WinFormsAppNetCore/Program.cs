2b3c8fb0c9b4597638bd88fbbd0e3d1a5528755b
Nikolay Pianikov <nikolayp@mail.ru>
2025-05-02 11:26:05 +0300

    WinForms finish

--- WinFormsAppNetCore/Program.cs
+++ WinFormsAppNetCore/Program.cs
@@ -6,6 +6,8 @@ public class Program(FormMain formMain)
     public static void Main()
     {
         ApplicationConfiguration.Initialize();
+        using var composition = new Composition();
+        composition.GetRoot(period: TimeSpan.FromSeconds(2)).Run();
     }
 
     private void Run() => Application.Run(formMain);
