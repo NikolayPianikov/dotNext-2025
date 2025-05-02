f343daf997938fbd3c9a3a0773fbe5992752e9df
Nikolay Pianikov <nikolayp@mail.ru>
2025-05-02 11:26:05 +0300

    WinForms finish

--- WinFormsAppNetCore/Composition.cs
+++ WinFormsAppNetCore/Composition.cs
@@ -4,6 +4,12 @@ internal partial class Composition
 {
     [Conditional("DI")]
     private void Setup() => DI.Setup()
+        .Hint(Hint.Resolve, "Off")
+
+        .RootArg<TimeSpan>("period")
+        .Root<Program>(nameof(GetRoot))
+
+        .Bind().As(Singleton).To<FormMain>()
         .Bind().As(Singleton).To<ClockViewModel>()
         .Bind().To<ClockModel>()
         .Bind().As(Singleton).To<Ticks>()
