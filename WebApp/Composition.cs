1db1ee64ac20672ebe94f787fefabc254426146a
Nikolay Pianikov <nikolayp@mail.ru>
2025-05-02 11:38:29 +0300

    Web finish

--- WebApp/Composition.cs
+++ WebApp/Composition.cs
@@ -1,9 +1,13 @@
 namespace WebApp;
 
-internal partial class Composition
+internal partial class Composition: ServiceProviderFactory<Composition>
 {
     [Conditional("DI")]
     private void Setup() => DI.Setup()
+        .DependsOn(Base)
+
+        .Roots<ControllerBase>()
+
         .Bind().As(Singleton).To<ClockViewModel>()
         .Bind().To<ClockModel>()
         .Bind().As(Singleton).To<Ticks>()
