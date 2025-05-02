06c35926e3938efa4b557484239dc6023b4f29f2
Nikolay Pianikov <nikolayp@mail.ru>
2025-05-02 11:54:02 +0300

    WebAPI finish

--- WebAPI/Controllers/ClockController.cs
+++ WebAPI/Controllers/ClockController.cs
@@ -2,12 +2,14 @@ namespace WebAPI.Controllers;
 
 [Route("[controller]")]
 public class ClockController(
-    ILogger<ClockController> logger) : ControllerBase
+    ILogger<ClockController> logger,
+    IAppViewModel app,
+    IClockViewModel clock) : ControllerBase
 {
     [HttpGet]
     public Task<ClockResult> GetDateTime()
     {
         logger.LogInformation(nameof(GetDateTime));
-        return Task.FromResult(new ClockResult("Title", "Date", "Time"));
+        return Task.FromResult(new ClockResult(app.Title, clock.Date, clock.Time));
     }
 }
